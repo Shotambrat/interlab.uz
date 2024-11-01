190b6182dce5e3598ebc313e78392451f91d5d89
musya <dudumikah@gmail.com>
2024-11-01 22:12:09 +0500

    feat: update: hhh

--- app/[locale]/_components/about/AboutBanner.js
+++ app/[locale]/_components/about/AboutBanner.js
@@ -3,16 +3,22 @@ import Image from "next/image";
 import { useTranslations } from "next-intl";
 
 export default function AboutBanner() {
-  const t = useTranslations()
+  const t = useTranslations();
   return (
     <>
       <div className="w-full h-screen-70 mdx:h-screen-90 relative max-slg:gap-5 flex flex-col pt-10">
         <div className="absolute max-slg:relative slg:top-1/4 lg:left-[7%] flex flex-col px-4 2xl:left-[5%]">
-          <h1 className="text-5xl max-mdx:text-3xl text-rose-400 font-bold w-full max-w-[560px]">{t('About.title')} <span className="text-black">{t('About.subtitle')}</span></h1>
+          <h1 className="text-5xl max-mdx:text-3xl text-rose-400 font-bold w-full max-w-[560px]">
+            {t("About.title")}{" "}
+            <span className="text-black">{t("About.subtitle")}</span>
+          </h1>
           <p className="w-full text-sm max-w-[400px] leading-5">
-          {t('About.description')}
+            {t("About.description")}
           </p>
-          <a href="tel:+998971504747" className="text-white bg-[#FB6A68] font-semibold self-start rounded-full px-8 py-3">
+          <a
+            href="tel:+998971504747"
+            className="text-white bg-[#FB6A68] font-semibold self-start rounded-full px-8 py-3"
+          >
             Связаться с нами
           </a>
         </div>
