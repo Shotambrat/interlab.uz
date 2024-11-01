601c04baa916bff457e86c5785c4203d0d253f97
musya <dudumikah@gmail.com>
2024-11-01 19:12:26 +0500

    feat: update: hhh

--- app/[locale]/_components/about/AboutBanner.js
+++ app/[locale]/_components/about/AboutBanner.js
@@ -1,4 +1,4 @@
-import aboutBanner from "@/public/images/interlab-logo.jpg";
+import aboutBanner from "@/public/images/image_2024-11-01_18-46-51.png";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 
@@ -7,9 +7,9 @@ export default function AboutBanner() {
   return (
     <>
       <div className="w-full h-screen-70 mdx:h-screen-90 relative max-slg:gap-5 flex flex-col pt-10">
-        <div className="absolute max-slg:relative slg:top-1/3 lg:left-[7%] flex flex-col px-4 2xl:left-[5%]">
+        <div className="absolute max-slg:relative slg:top-1/4 lg:left-[7%] flex flex-col px-4 2xl:left-[5%]">
           <h1 className="text-5xl max-mdx:text-3xl text-rose-400 font-bold w-full max-w-[560px]">{t('About.title')} <span className="text-black">{t('About.subtitle')}</span></h1>
-          <p className="w-full max-w-[400px] leading-5">
+          <p className="w-full text-sm max-w-[400px] leading-5">
           {t('About.description')}
           </p>
           <a href="tel:+998971504747" className="text-white bg-[#FB6A68] font-semibold self-start rounded-full px-8 py-3">
