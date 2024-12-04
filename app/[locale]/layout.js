a7b1d4b9d01dc68a295f70bdf8b7715c96758198
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-12-04 17:11:38 +0500

    @animation

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -43,45 +43,45 @@ export default async function LocaleLayout({ children, params }) {
         </Script>
 
         <NextIntlClientProvider locale={locale} messages={messages}>
-          <Header locale={locale} />
           <PageTransition>
+            <Header locale={locale} />
             {children}
+            <CallToAction locale={locale} />
+            <Footer locale={locale} />
           </PageTransition>
-          <CallToAction locale={locale} />
-          {/* Яндекс.Метрика */}
-          <Script
-            id='yandex-metrika'
-            strategy='afterInteractive'
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-              ym(98667284, "init", {
-                  clickmap:true,
-                  trackLinks:true,
-                  accurateTrackBounce:true,
-                  webvisor:true,
-                  ecommerce:"dataLayer"
-              });
-              `
-            }}
-          />
-          <noscript>
-            <div>
-              <img
-                src='https://mc.yandex.ru/watch/98667284'
-                style={{ position: 'absolute', left: '-9999px' }}
-                alt=''
-              />
-            </div>
-          </noscript>
-
-          <Footer locale={locale} />
         </NextIntlClientProvider>
+
+        {/* Яндекс.Метрика */}
+        <Script
+          id='yandex-metrika'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(98667284, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                ecommerce:"dataLayer"
+            });
+            `
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src='https://mc.yandex.ru/watch/98667284'
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=''
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   )
