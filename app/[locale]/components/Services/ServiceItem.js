8f25d2057c68f9573736927a4a20392dcbab663c
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-12-04 16:42:49 +0500

    @tailwind service card application centered and laptop responsive

--- app/[locale]/components/Services/ServiceItem.js
+++ app/[locale]/components/Services/ServiceItem.js
@@ -5,8 +5,8 @@ import { useLocale, useTranslations } from 'next-intl';
 import { useState } from 'react';
 import ContactWithUs from '../Modals/OnlineReq';
 
-export default function ServiceItems({ title , application }) {
-  const [open , setOpen] = useState(false)
+export default function ServiceItems({ title, application }) {
+  const [open, setOpen] = useState(false)
 
   const t = useTranslations();
   const locale = useLocale()
@@ -15,27 +15,27 @@ export default function ServiceItems({ title , application }) {
 
 
   return (
-    <div  className="w-full h-[200px] cursor-pointer slg:h-[200px] rounded-3xl pl-6 py-6 bg-slate-100">
+    <div className="w-full h-[200px] cursor-pointer slg:h-[200px] rounded-3xl pl-6 py-6 bg-slate-100">
       <div className="flex flex-col h-full justify-between items-start">
         <div className="flex flex-col">
           <h2 className="font-bold text-xl slg:text-2xl break-words">{title}</h2>
           {/* Display the category name */}
           {/* <p className="text-neutral-400">{category}</p> */}
         </div>
-        
+
         {/* PEREYTI */}
         {application && (
-          <div className='w-full flex flex-col  mdl:flex-row gap-[10px]'>
-          <Link href={`tel:+998977578822`} className='bg-[#FB6A68] py-[12px] px-[16px] text-white rounded-full mdl:py-[13px] mdl:px-[25px] font-bold'>
+          <div className='w-[93%] flex flex-col  mdl:flex-row gap-[10px]'>
+            <Link href={`tel:+998977578822`} className='bg-[#FB6A68] text-center py-[12px] px-[16px] text-white rounded-full mdl:py-[13px] mdl:px-[20px] font-bold'>
               {locale === 'ru' ? 'Позвонить' : 'Telefon qilish'}
-          </Link>
-          <button onClick={handleOpen} className=' text-[#FB6A68] rounded-full py-[12px] px-[16px] border border-[#FB6A68] mdl:py-[13px] mdl:px-[25px] font-bold'>
+            </Link>
+            <button onClick={handleOpen} className=' text-[#FB6A68] rounded-full py-[12px] px-[16px] border border-[#FB6A68] mdl:py-[13px] mdl:px-[20px] font-bold'>
               {locale === 'ru' ? 'Оставить заявку' : 'Ariza yuborish'}
-          </button>
-       </div>
+            </button>
+          </div>
         )}
-      {open ? <ContactWithUs setState={setOpen} /> : null}
-       
+        {open ? <ContactWithUs setState={setOpen} /> : null}
+
       </div>
     </div>
   );
