10cbd6162f7693e275f415eb4c8f772616eff3d7
kidsan1 <samatrustem@gmail.com>
2025-01-09 19:02:28 +0500

    doctor

--- app/[locale]/components/Doctors/Address.js
+++ app/[locale]/components/Doctors/Address.js
@@ -17,19 +17,7 @@ export default function Address() {
         >
           INTERMED INNOVATION
         </a>
-        {/* <a
-          href="https://yandex.uz/maps/10335/tashkent/category/medical_center_clinic/184106108/?utm_medium=mapframe&utm_source=maps"
-          style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '14px' }}
-        >
-          {locale  === 'ru' ? ' Медцентр, клиника в Ташкенте' : ''}
-
-        </a> */}
-        {/* <a
-          href="https://yandex.uz/maps/10335/tashkent/category/medical_laboratory/184106078/?utm_medium=mapframe&utm_source=maps"
-          style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '28px' }}
-        >
-          {locale  === 'ru' ? ' Медицинская лаборатория в Ташкенте' : 'Tiibiyot laboratoriya'}
-        </a> */}
+     
         <iframe
           src="https://yandex.uz/map-widget/v1/?ll=69.291355%2C41.351735&mode=poi&poi%5Bpoint%5D=69.291036%2C41.350889&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D173898485881&z=17.65"
           width="100%"
